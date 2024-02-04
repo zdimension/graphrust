38b2664c09da3052019aad490446d16e90c9b640
Tom Niget <zippedfire@free.fr>
2024-02-04 01:22:04 +0100

    Show milliseconds in log

--- viewer/src/main.rs
+++ viewer/src/main.rs
@@ -15,7 +15,7 @@ fn main() -> eframe::Result<()> {
             writeln!(
                 buf,
                 "[{}] [{}:{}] {}",
-                chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                chrono::Local::now().format("%Y-%m-%d %H:%M:%S.%3f"),
                 record.file().unwrap_or("unknown"),
                 record.line().unwrap_or(0),
                 record.args()
