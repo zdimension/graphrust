d1f52b497e6e10aa299c10e7d30c03049790e3c5
Tom Niget <zippedfire@free.fr>
2024-02-23 19:47:51 +0100

    Switch to GPU for Louvain too

--- import_neo4j/src/main.rs
+++ import_neo4j/src/main.rs
@@ -1,18 +1,16 @@
-mod gephi;
-mod louvain;
-use crate::gephi::Modularity;
-
 use ahash::AHashMap;
 use colourado::{ColorPalette, PaletteType};
 use derivative::Derivative;
 use figment::providers::{Env, Format, Toml};
 use figment::Figment;
+use std::ffi::{CStr, OsStr};
+use std::process::{Command, ExitStatus};
 
 use graph_format::*;
 use neo4rs::{query, ConfigBuilder, Graph};
 use petgraph::graph::NodeIndex;
 use serde::Deserialize;
-use std::io::Write;
+use std::io::{BufRead, BufReader, Write};
 use std::sync::Mutex;
 
 #[derive(Deserialize, Derivative)]
@@ -33,6 +31,8 @@ struct Config {
     threads: usize,
     #[derivative(Default(value = "1024"))]
     chunk_size: usize,
+    #[derivative(Default(value = "0.01"))]
+    community_min_gain: f32,
 }
 
 static LAST_LOG_TIME: Mutex<std::time::Instant> =
@@ -55,6 +55,111 @@ macro_rules! log
     }
 }
 
+fn run_command(cmd: &mut Command) -> ExitStatus {
+    let mut res = cmd.stdout(std::process::Stdio::piped()).spawn().unwrap();
+    if let Some(stdout) = res.stdout.take() {
+        let reader = BufReader::new(stdout);
+        for line in reader.lines() {
+            println!("{}", line.unwrap());
+        }
+    }
+    res.wait().unwrap()
+}
+
+fn do_layout(file: &mut GraphFile, config: &Config) {
+    log!(
+        "ssh exited with: {}\r\n",
+        run_command(Command::new("ssh").arg("zdimension@domino").arg(format!(
+            r"
+            cd /home/zdimension/graphrust_tools/GPUGraphLayout/builds/linux;
+            rm *.bin;
+            ./graph_viewer gpu {} 1 sg 1 1 approximate ../../../edges.txt . bin",
+            config.layout_iterations
+        )))
+    );
+    log!(
+        "scp exited with: {}",
+        run_command(Command::new("scp")
+            .arg(format!(
+                "zdimension@domino:/home/zdimension/graphrust_tools/GPUGraphLayout/builds/linux/edges.txt_{}.bin",
+                config.layout_iterations
+            ))
+            .arg("layout.bin")
+            )
+    );
+
+    #[derive(Readable)]
+    struct GGLNode {
+        id: u32,
+        x: f32,
+        y: f32,
+    }
+    #[derive(Readable)]
+    struct GGLFile {
+        #[speedy(length =..)]
+        nodes: Vec<GGLNode>,
+    }
+
+    for layout_node in GGLFile::read_from_file("layout.bin")
+        .unwrap()
+        .nodes
+        .into_iter()
+    {
+        file.nodes[layout_node.id as usize].position = Point {
+            x: layout_node.x,
+            y: layout_node.y,
+        };
+    }
+
+    log!("Layout done");
+}
+
+fn do_modularity(file: &mut GraphFile, config: &Config) {
+    log!(
+        "ssh exited with: {}",
+        run_command(Command::new("ssh").arg("zdimension@domino").arg(format!(
+            r"
+            cd /home/zdimension/graphrust_tools/gpu-louvain;
+            rm *.bin;
+            ./gpulouvain -f ../edges.txt -g {}",
+            config.community_min_gain
+        )))
+    );
+    log!(
+        "scp exited with: {}",
+        run_command(
+            Command::new("scp")
+                .arg("zdimension@domino:/home/zdimension/graphrust_tools/gpu-louvain/comms.bin")
+                .arg("comms.bin")
+        )
+    );
+    #[derive(Readable)]
+    struct GPULouvainFile {
+        num_comms: u16,
+        #[speedy(length =..)]
+        nodes: Vec<u16>,
+    }
+
+    let comm_file = GPULouvainFile::read_from_file("comms.bin").unwrap();
+
+    log!("Creating color palette");
+    let top_comms = (comm_file.num_comms as f32 * 0.1).ceil() as u16;
+    let top_palette = ColorPalette::new(top_comms as u32, PaletteType::Random, false);
+    let rest_comms = comm_file.num_comms - top_comms;
+    let rest_palette = ColorPalette::new(rest_comms as u32, PaletteType::Random, false);
+    let colors = top_palette.colors.iter().chain(rest_palette.colors.iter());
+
+    file.classes.extend(colors.map(|color| Color3b {
+        r: (color.red * 255.0) as u8,
+        g: (color.green * 255.0) as u8,
+        b: (color.blue * 255.0) as u8,
+    }));
+
+    for (i, comm) in comm_file.nodes.iter().copied().enumerate() {
+        file.nodes[i].class = comm;
+    }
+}
+
 #[tokio::main]
 async fn main() {
     *LAST_LOG_TIME.lock().unwrap() = std::time::Instant::now();
@@ -68,10 +173,10 @@ async fn main() {
     log!("Using config: {:#?}", config);
 
     let n4j_config = ConfigBuilder::default()
-        .uri(config.uri)
-        .user(config.user)
-        .password(config.pass)
-        .fetch_size(1048576)
+        .uri(&config.uri)
+        .user(&config.user)
+        .password(&config.pass)
+        .fetch_size(10485760)
         .build()
         .unwrap();
     log!("Connecting");
@@ -123,14 +228,22 @@ async fn main() {
     while let Ok(Some(row)) = edges_q.next().await {
         let uid1: String = row.get("n.uid").unwrap();
         let uid2: String = row.get("m.uid").unwrap();
-        let a = *nodes_ids.get(&uid1).expect(&uid1);
-        let b = *nodes_ids.get(&uid2).expect(&uid2);
+        /*let a = *nodes_ids.get(&uid1).expect(&uid1);
+        let b = *nodes_ids.get(&uid2).expect(&uid2);*/
+        let Some(&a) = nodes_ids.get(&uid1) else {
+            log!("Node not found: {}", uid1);
+            continue;
+        };
+        let Some(&b) = nodes_ids.get(&uid2) else {
+            log!("Node not found: {}", uid2);
+            continue;
+        };
         edges.push((a, b));
         file.edges.push(EdgeStore {
             a: a as u32,
             b: b as u32,
         });
-        writeln!(&mut edges_writer, "{} {}", a, b).unwrap();
+        //writeln!(&mut edges_writer, "{} {}", a, b).unwrap();
     }
     log!("{} edges", edges.len());
 
@@ -152,86 +265,38 @@ async fn main() {
             }
         }
     }
-    assert_eq!(
+    /*assert_eq!(
         count,
         adj.len(),
-        "Graph contains {} unconnected nodes",
-        adj.len() - count
-    );
+        "Graph contains {} unconnected nodes: {}",
+        adj.len() - count,
+        covered
+            .iter()
+            .enumerate()
+            .filter(|(_, &c)| !c)
+            .map(|(i, _)| i)
+            .map(|i| unsafe {
+                CStr::from_ptr(file.ids.as_ptr().add(file.nodes[i].offset_id as usize) as *const _)
+            }
+            .to_str()
+            .unwrap())
+            .map(|id| format!("bfs('{}', level=1, limit=10)", id))
+            .collect::<Vec<_>>()
+            .join("\n")
+    );*/
 
-    log!("Running Louvain algorithm2");
-    let mut pet = petgraph::Graph::new_undirected();
-    let mut inv_map: AHashMap<usize, NodeIndex> = AHashMap::new();
-    for i in 0..adj.len() {
-        inv_map.insert(i, pet.add_node(()));
-    }
-    for (i, pers) in adj.iter().enumerate() {
-        for &j in pers {
-            let i = *inv_map.get(&i).unwrap();
-            let j = *inv_map.get(&(j as usize)).unwrap();
-            pet.add_edge(i, j, 1.0);
-        }
-    }
-    let resolution = 1.0;
-    let noise = 1;
-    let mut modularity = Modularity::new(resolution, noise);
-    log!("Running clusterization");
-    let results = modularity.execute(&pet);
-    log!("Done");
-    let num_of_communities = *modularity.communityByNode.iter().max().unwrap_or(&0) as usize + 2; // Parent Community included
-    println!(
-        "Number of Clusters: {} -  with resolution {}",
-        num_of_communities - 1,
-        resolution
-    );
-    println!("Final Modularity: {:?}", results);
-
-    log!("Creating color palette");
-    let palette = ColorPalette::new(num_of_communities as u32, PaletteType::Random, false);
-    file.classes.push(Color3b { r: 255, g: 0, b: 0 });
-
-    file.classes
-        .extend(palette.colors.iter().map(|color| Color3b {
-            r: (color.red * 255.0) as u8,
-            g: (color.green * 255.0) as u8,
-            b: (color.blue * 255.0) as u8,
-        }));
-
-    for (i, comm) in modularity.communityByNode.iter().enumerate() {
-        file.nodes[i].class = *comm as u16 + 1;
+    writeln!(&mut edges_writer, "{} {}", file.nodes.len(), edges.len()).unwrap();
+    for EdgeStore { a, b } in file.edges.iter() {
+        writeln!(&mut edges_writer, "{} {}", a, b).unwrap();
     }
 
-    // run scp edges.txt zdimension@domino:/home/zdimension/GPUGraphLayout/builds/linux
-    use std::process::Command;
+    log!("Wrote edges file");
+
     log!(
-        "scp exited with: {}",
+        "Edges file copied; scp exited with: {}",
         Command::new("scp")
             .arg("edges.txt")
-            .arg("zdimension@domino:/home/zdimension/GPUGraphLayout/builds/linux")
-            .stdout(std::process::Stdio::null())
-            .spawn()
-            .unwrap()
-            .wait()
-            .unwrap()
-    );
-    log!(
-        "ssh exited with: {}",
-        Command::new("ssh")
-            .arg("zdimension@domino")
-            .arg(format!("cd /home/zdimension/GPUGraphLayout/builds/linux; rm *.bin; ./graph_viewer gpu {} 1 sg 1 1 approximate edges.txt . bin", config.layout_iterations))
-            .spawn()
-            .unwrap()
-            .wait()
-            .unwrap()
-    );
-    log!(
-        "scp exited with: {}",
-        Command::new("scp")
-            .arg(format!(
-                "zdimension@domino:/home/zdimension/GPUGraphLayout/builds/linux/edges.txt_{}.bin",
-                config.layout_iterations
-            ))
-            .arg("edges.bin")
+            .arg("zdimension@domino:/home/zdimension/graphrust_tools")
             .stdout(std::process::Stdio::null())
             .spawn()
             .unwrap()
@@ -239,95 +304,9 @@ async fn main() {
             .unwrap()
     );
 
-    #[derive(Readable)]
-    struct GGLNode {
-        id: u32,
-        x: f32,
-        y: f32,
-    }
-    #[derive(Readable)]
-    struct GGLFile {
-        #[speedy(length = ..)]
-        nodes: Vec<GGLNode>,
-    }
+    do_layout(&mut file, &config);
 
-    for layout_node in GGLFile::read_from_file("edges.bin")
-        .unwrap()
-        .nodes
-        .into_iter()
-    {
-        file.nodes[layout_node.id as usize].position = Point {
-            x: layout_node.x,
-            y: layout_node.y,
-        };
-    }
-
-    log!("Layout done");
-
-    /*rayon::ThreadPoolBuilder::new()
-        .num_threads(config.threads)
-        .build_global()
-        .ok();
-
-    let mut layout = Layout::<f32>::from_graph(
-        edges,
-        Nodes::Degree(file.nodes.len()),
-        None,
-        None,
-        Settings {
-            //barnes_hut: Some(1.2),
-            barnes_hut: None,
-            chunk_size: Some(config.chunk_size),
-            dimensions: 2,
-            dissuade_hubs: false,
-            ka: 0.1,
-            kg: 0.1,
-            kr: 0.02,
-            lin_log: false,
-            speed: 1.0,
-            prevent_overlapping: None,
-            strong_gravity: false,
-        },
-    );
-
-    let start = std::time::Instant::now();
-    log!("Running layout");
-    for i in 0..config.layout_iterations {
-        layout.iteration();
-        if i % (config.layout_iterations / 10) == 0 {
-            log!("Iteration {}", i);
-        }
-    }
-    log!(
-        "{} iterations in {:?}",
-        config.layout_iterations,
-        start.elapsed()
-    );
-
-    log!("Writing positions");
-    for (i, p) in layout.points.iter().enumerate() {
-        file.nodes[i].position = Point { x: p[0], y: p[1] };
-    }*/
-
-    /*log!("Running Louvain algorithm");
-    let louvain = louvain::Graph::new(adj).louvain();
-
-    log!("Creating color palette");
-    let palette = ColorPalette::new(louvain.nodes.len() as u32, PaletteType::Random, false);
-    file.classes.push(Color3b { r: 255, g: 0, b: 0 });
-    for (i, (comm, color)) in louvain.nodes.iter().zip(palette.colors).enumerate() {
-        for user in comm.payload.as_ref().unwrap() {
-            file.nodes[user.0].class = (i + 1) as u16;
-        }
-        file.classes.push(Color3b {
-            r: (color.red * 255.0) as u8,
-            g: (color.green * 255.0) as u8,
-            b: (color.blue * 255.0) as u8,
-        });
-    }
-    for node in &file.nodes {
-        assert!(node.class != 0);
-    }*/
+    do_modularity(&mut file, &config);
 
     log!("Writing metadata");
 
