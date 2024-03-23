26a5707ea0689fe7c04e9e0814676bc4b27c6cc7
Tom Niget <zippedfire@free.fr>
2024-03-23 18:48:13 +0100

    Add code for bfs

--- plots/src/main.rs
+++ plots/src/main.rs
@@ -1,12 +1,50 @@
 use ahash::AHashMap;
 use graph_format::{GraphFile, Readable};
 use inline_python::python;
+use rayon::prelude::*;
 use std::collections::hash_map::Entry::{Occupied, Vacant};
+
+#[macro_export]
+macro_rules! log
+{
+    ($($arg:tt)*) =>
+    {
+        {
+            let now = std::time::Instant::now();
+            println!("[{}] [{}:{}] {}", chrono::Local::now().format("%Y-%m-%d %H:%M:%S.%3f"),
+                file!(), line!(), format_args!($($arg)*));
+        }
+    }
+}
 
 fn main() {
     let file = GraphFile::read_from_file("graph_n4j.bin").unwrap();
+
     let mut degrees = AHashMap::new();
     let persons = file.get_adjacency();
+
+    /*let max_dist = |start_node| {
+        let mut visited = vec![false; persons.len()];
+        let mut queue = std::collections::VecDeque::new();
+        queue.push_back((start_node as usize, 0));
+        let mut max_dist = 0;
+        while let Some((node, dist)) = queue.pop_front() {
+            if visited[node] {
+                continue;
+            }
+            visited[node] = true;
+            max_dist = dist;
+            for &neigh in &persons[node] {
+                queue.push_back((neigh as usize, dist + 1));
+            }
+        }
+        max_dist
+    };
+    log!("Getting bfs");
+    let v = (0..100).into_par_iter().map(max_dist).max().unwrap();
+    log!("Bfs from all nodes: {}", v);
+    return;*/
+
     println!(
         "Average degree: {}",
         persons.iter().map(|p| p.len()).sum::<usize>() as f64 / persons.len() as f64
