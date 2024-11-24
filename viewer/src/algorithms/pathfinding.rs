9e1f37c4932b95d34ebe19e0cd8fbeeb118dff20
Tom Niget <zippedfire@free.fr>
2024-11-24 23:45:25 +0100

    Balance the bidirectional BFS (500ms → 10ms for long paths)
    Thanks for https://arxiv.org/pdf/2410.22186

--- viewer/src/algorithms/pathfinding.rs
+++ viewer/src/algorithms/pathfinding.rs
@@ -30,15 +30,11 @@ pub fn do_pathfinding(
     let mut visited_b = BitSet::with_capacity(data.len());
     let mut pred_f = vec![None; data.len()];
     let mut pred_b = vec![None; data.len()];
-    let mut dist_f = vec![i32::MAX; data.len()];
-    let mut dist_b = vec![i32::MAX; data.len()];
 
     visited_f.insert(src_id);
     visited_b.insert(dest_id);
     queue_f.push_back(src_id);
     queue_b.push_back(dest_id);
-    dist_f[src_id] = 0;
-    dist_b[dest_id] = 0;
 
     let bfs = |current: usize,
                queue: &mut VecDeque<usize>,
@@ -72,37 +68,40 @@ pub fn do_pathfinding(
     };
 
     let intersect = 'main: loop {
-        let mut queue_new_f = VecDeque::new();
-        while let Some(id_f) = queue_f.pop_front() {
-            if let Some(inter) = bfs(
-                id_f,
-                &mut queue_new_f,
-                &mut visited_f,
-                &mut pred_f,
-                &visited_b,
-            ) {
-                break 'main inter;
+        // Balancing the bidirectional BFS (instead of visiting each k-neighborhood alternatively)
+        // shortens the usual runtime on my machine for long paths (>11) from 500ms to 10ms.
+        // Thanks to https://arxiv.org/pdf/2410.22186
+        if visited_b.len() < visited_f.len() && !queue_b.is_empty() {
+            let mut queue_new_b = VecDeque::new();
+            while let Some(id_b) = queue_b.pop_front() {
+                if let Some(inter) = bfs(
+                    id_b,
+                    &mut queue_new_b,
+                    &mut visited_b,
+                    &mut pred_b,
+                    &visited_f,
+                ) {
+                    break 'main inter;
+                }
             }
-        }
-
-        let mut queue_new_b = VecDeque::new();
-        while let Some(id_b) = queue_b.pop_front() {
-            if let Some(inter) = bfs(
-                id_b,
-                &mut queue_new_b,
-                &mut visited_b,
-                &mut pred_b,
-                &visited_f,
-            ) {
-                break 'main inter;
+            queue_b = queue_new_b;
+        } else if !queue_f.is_empty() {
+            let mut queue_new_f = VecDeque::new();
+            while let Some(id_f) = queue_f.pop_front() {
+                if let Some(inter) = bfs(
+                    id_f,
+                    &mut queue_new_f,
+                    &mut visited_f,
+                    &mut pred_f,
+                    &visited_b,
+                ) {
+                    break 'main inter;
+                }
             }
-        }
-
-        if queue_new_f.is_empty() && queue_new_b.is_empty() {
+            queue_f = queue_new_f;
+        } else {
             return None;
         }
-        queue_f = queue_new_f;
-        queue_b = queue_new_b;
     };
 
     let mut path = vec![intersect];
